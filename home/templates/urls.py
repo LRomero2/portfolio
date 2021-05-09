3b3c7e4fecfafb8b8c6bd8521da0c587611654b8
LRomero2 <lauren.romero@outlook.com>
2021-05-09 15:48:07 +0000

    Updated home app and deleted old one.

--- home/templates/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('home/', include('home.urls')),
-    path('about/', include('about.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
