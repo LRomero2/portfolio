d33aacf25acae6b00ada4f6cbbe0eb12b772603c
LRomero2 <lauren.romero@outlook.com>
2021-05-14 10:59:13 +0000

    Rearranged files properly.

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("index.html",)
+@app.route("/index")
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
